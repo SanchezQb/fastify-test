9133b5ce1e54b758920563222f7dc1b75227bb09
Punts Ville <puntsville@Puntss-MacBook-Pro.local>
2019-12-03 13:46:08 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const fastify = require('fastify')({ logger: true })
+
+const appDetails = require('./appDetails')
+const fileDetails = require('./fileDetails')
+
+// Declare a route
+fastify.get('/app', async (request, reply) => {
+  return appDetails;
+})
+
+fastify.get('/file', async (request, reply) => {
+    return fileDetails;
+  })
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
